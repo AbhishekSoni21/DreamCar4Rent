01a883969132369950274a5b2e7f3031953dd4e0
Abhishek Soni <abhiputulshek@gmail.com>
2021-09-18 23:52:48 +0530

    initial setup for auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  loginMode=true;
+  form!:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.form =this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6)]]
+
+    })
+  }
+
+  public onModeChange():void{
+    this.loginMode=!this.loginMode
+  }
+
+  getControl(value:string):boolean|undefined{
+    return  this.form.get(value)?.touched && !this.form.get(value)?.valid
+  }
+
+  onSubmit():void{
+    if(this.form.valid){
+      console.log("value",this.form.value)
+
+    }
   }
 
 }
