b062e21d7eb95bcac2c7335dbda56af6ceabc055
Abhishek Soni <abhiputulshek@gmail.com>
2021-10-04 17:12:00 +0530

    deployment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCOiNhEsE6IuiHgeKhxmQ3XEVNjZjGL2G0",
+    authDomain: "dreamcar4u2rent.firebaseapp.com",
+    databaseURL: "https://dreamcar4u2rent-default-rtdb.firebaseio.com",
+    projectId: "dreamcar4u2rent",
+    storageBucket: "dreamcar4u2rent.appspot.com",
+    messagingSenderId: "182399039300",
+    appId: "1:182399039300:web:523d8bc7c03466e5c94911",
+    measurementId: "G-G15XN0Y146"
+  }
 };
